c67544d150987c9ec756f0dfe0acb0d43d856188
Flavio Poletti <flavio@polettix.it>
2015-04-25 23:43:23 +0200

    updates in the documentation updater

--- update-docs.sh
+++ update-docs.sh
@@ -2,13 +2,12 @@
 ME=$(readlink -f "$0")
 MYDIR=$(dirname "$ME")
 cd $MYDIR &&
-[ -e 'wrapperl' ] &&
-pod2markdown wrapperl README.md &&
-contents=$(kramdown README.md) &&
-here=$(git reflog | perl -nale 'print$F[0];last') &&
+branch=$(git branch | sed -n 's/^* //p') &&
+[ "$branch" == "master" ] &&
+contents=$(pod2markdown wrapperl | kramdown) &&
 git checkout gh-pages &&
 echo "$contents" > index.html &&
 (
    git commit index.html -m 'aligned documentation'
-   git checkout "$here"
+   git checkout master
 )
